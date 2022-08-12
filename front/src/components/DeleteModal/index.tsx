bcd21564b2f9a6f28c91c05488e178ff15112dfd
Gustavo Willemann <guswillemann@gmail.com>
2022-08-11 23:09:50 -0300

    add DeleteModal to user list delete action

---
+++ front/src/components/DeleteModal/index.tsx
@@ -0,0 +1,111 @@
+import styled from '@emotion/styled';
+import CloseIcon from '@mui/icons-material/Close';
+import { Box, Button, IconButton, Paper, Typography } from '@mui/material';
+import { FC, MouseEvent as ReactMouseEvent, ReactNode, useEffect, useRef } from 'react';
+
+const DeleteModalWrapper = styled.dialog`
+  position: relative;
+  background: none;
+  border: none;
+  padding: 0;
+
+  .modal-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 300px;
+    min-height: 200px;
+    padding: 40px;
+  }
+  
+  .close-btn {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+  }
+`
+
+type Props = {
+  onClose: () => void
+  onDelete: () => void
+  isOpen: boolean
+}
+
+const DeleteModal: FC<Props> = ({ onClose, onDelete, isOpen }) => {
+  const ref = useRef<HTMLDialogElement>(null)
+
+  useEffect(() => {
+    const currentModal = ref.current
+    if (!currentModal) return
+
+    if (isOpen) currentModal.showModal()
+    else currentModal.close()
+    
+    return () => currentModal.close()
+  }, [isOpen])
+
+  const handleDialogClick = (e: ReactMouseEvent<HTMLDialogElement, MouseEvent>) => {
+    const currentModal = ref.current
+    if (!currentModal) return
+    
+    const dialogRect = currentModal.getBoundingClientRect()
+
+    const isDialogClick = (
+      dialogRect.top <= e.clientY &&
+      e.clientY <= dialogRect.bottom &&
+      dialogRect.left <= e.clientX &&
+      e.clientX <= dialogRect.right
+    );
+    
+    if (!isDialogClick) ref.current.close()
+  }
+
+  const handleClose = () => {
+    ref.current?.close()
+  }
+
+  return (
+    <DeleteModalWrapper
+      ref={ref}
+      onClose={onClose}
+      onClick={handleDialogClick}
+    >
+      <Paper className='modal-container'>
+        <IconButton
+          className='close-btn'
+          size="small"
+          onClick={handleClose}
+        >
+          <CloseIcon />
+        </IconButton>
+        <Typography variant="h4">
+          Do you really want to delete this entry?
+        </Typography>
+        <Box
+          display="flex"
+          gap="20px"
+          sx={{
+            mt: 'auto',
+            ml: 'auto',
+          }}
+        >
+          <Button
+            variant="contained"
+            onClick={handleClose}
+          >
+            cancel
+          </Button>
+          <Button
+            color="warning"
+            variant="contained"
+            onClick={onDelete}
+          >
+            delete
+          </Button>
+        </Box>
+      </Paper>
+    </DeleteModalWrapper>
+  )
+}
+
+export default DeleteModal
--- front/src/components/UserTableEntry/index.tsx
+++ front/src/components/UserTableEntry/index.tsx
@@ -3,27 +3,28 @@ import { IconButton, TableCell, TableRow } from '@mui/material'
 import { FC, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { User } from '../../types'
+import DeleteModal from '../DeleteModal'
 
 const UserTableEntry: FC<{ user: User }> = ({ user }) => {
   const { id, name, status } = user
   const navigate = useNavigate()
-  const [rowClickEnabled, setRowClickEnabled] = useState(true)
 
+  const [rowClickEnabled, setRowClickEnabled] = useState(true)
+  const allowRowClick = () => setRowClickEnabled(true)
+  const disallowRowClick = () => setRowClickEnabled(false)
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
+  
   const handleRowClick = () => {
-    if (!rowClickEnabled) return
+    if (!rowClickEnabled || isModalOpen) return
     navigate('/dash/user/' + id)
   }
 
   const handleDelete = () => {
     console.log('delete', name)
-  }
-
-  const handleDeleteMouseEnter = () => {
-    setRowClickEnabled(false)
-  }
-  
-  const handleDeleteMouseLeave = () => {
-    setRowClickEnabled(true)
+    closeModal()
   }
 
   return (
@@ -37,12 +38,17 @@ const UserTableEntry: FC<{ user: User }> = ({ user }) => {
       <TableCell>{status}</TableCell>
       <TableCell align="right">
         <IconButton
-          onClick={handleDelete}
-          onMouseEnter={handleDeleteMouseEnter}
-          onMouseLeave={handleDeleteMouseLeave}
+          onClick={openModal}
+          onMouseEnter={disallowRowClick}
+          onMouseLeave={allowRowClick}
         >
           <DeleteIcon color="warning" />
         </IconButton>
+        <DeleteModal
+          isOpen={isModalOpen}
+          onDelete={handleDelete}
+          onClose={closeModal}
+        />
       </TableCell>
     </TableRow>
   )
