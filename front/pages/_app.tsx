e419e75b7bad2fc93dc9f27d95ec2711d9e19ba9
Gustavo Willemann <guswillemann@gmail.com>
2022-09-05 14:00:34 -0300

    add Roboto font

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
