e56bcfd91165a9d90ccb3744b08828e7b1b66fc9
Gustavo Willemann <guswillemann@gmail.com>
2022-09-05 14:33:56 -0300

    fix google font import

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,16 +1,8 @@
 import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"></link>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
