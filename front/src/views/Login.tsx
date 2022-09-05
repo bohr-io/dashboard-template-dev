792751b249e1c14c3a130ab095918377f7d1a82c
Gustavo Willemann <guswillemann@gmail.com>
2022-09-05 14:15:38 -0300

    change Login view layout

--- front/src/views/Login.tsx
+++ front/src/views/Login.tsx
@@ -1,12 +1,12 @@
-import { Button, TextField, Typography } from '@mui/material'
+import { Box, Button, Paper, TextField, Typography } from '@mui/material'
 import Head from 'next/head'
 import { FC, FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Reaptcha from 'reaptcha'
+import DashInput from '../components/DashInput'
 import useToast from '../hooks/useToast'
+import HomeIcon from '../icons/HomeIcon'
 import loginResource from '../services/api/loginResource'
-
-import styles from '../styles/Home.module.css'
 
 const REACAPTCHA_SITE_KEY = "6Lc2OG4hAAAAAF_Wx9HXq3O-FKTmyG_eOamPyykl"
 
@@ -16,7 +16,7 @@ const Login: FC = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [recaptchaToken, setRecaptchaToken] = useState('')
-  const isLoginDisabled = !recaptchaToken
+  const isLoginDisabled = !username || !password || !recaptchaToken
 
   const updateRecaptchaToken = (token: string) => setRecaptchaToken(token)
   const clearRecaptchaToken = () => setRecaptchaToken('')
@@ -37,56 +37,79 @@ const Login: FC = () => {
   }
   
   return (
-    <div className={styles.container}>
+    <Box
+      sx={{
+        display: 'grid',
+        placeItems: 'center',
+        minHeight: '100vh',
+        backgroundColor: 'primary.main',
+      }}
+    >
       <Head>
         <title>Dashboard</title>
         <meta name="description" content="Dashboard" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Typography variant="h1">
-          Dashboard - Login
+      <Paper
+        component="main"
+        sx={{
+          borderRadius: '8px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          p: '54px 30px',
+        }}
+      >
+        <HomeIcon fontSize="large" />
+        <Typography variant="h1" sx={{ fontSize: '24px', mt: '22px', mb: '4px' }}>
+          Dashboard login
+        </Typography>
+        <Typography variant="subtitle1" sx={{ color: 'secondary.main', mt: '4px', mb: '35px' }}>
+          Enter your username and password
         </Typography>
 
-        <form
+        <Box
+          component="form"
+          name="login-form"
           onSubmit={handleSubmit}
-          className={styles.loginForm}
+          sx={{ display: 'flex', flexDirection: 'column', gap: '24px' }}
         >
-          <TextField
+          <DashInput
             id="user"
             label="User"
+            value={username}
             type="text"
             size="small"
-            variant="standard"
-            value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
 
-          <TextField
+          <DashInput
             id="password"
             label="Password"
             type="password"
             size="small"
-            variant="standard"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <Reaptcha
-            sitekey={REACAPTCHA_SITE_KEY}
-            onVerify={updateRecaptchaToken}
-            onExpire={clearRecaptchaToken}
-          />
+          <Box width={304} height={78}>
+            <Reaptcha
+              sitekey={REACAPTCHA_SITE_KEY}
+              onVerify={updateRecaptchaToken}
+              onExpire={clearRecaptchaToken}
+            />
+          </Box>
           <Button
             type="submit"
             disabled={isLoginDisabled}
+            variant="contained"
           >
             Login
           </Button>
-        </form>
-      </main>
+        </Box>
+      </Paper>
       <Toast />
-    </div>
+    </Box>
   )
 }
 
