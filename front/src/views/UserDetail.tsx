f9293aff86f62819e03dd47de86abb37fcfb6892
Gustavo Willemann <guswillemann@gmail.com>
2022-09-08 15:33:31 -0300

    adjust UserDetails layout

--- front/src/views/UserDetail.tsx
+++ front/src/views/UserDetail.tsx
@@ -1,7 +1,8 @@
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { Button, Card, CardActions, CardContent, TextField, Typography } from '@mui/material';
+import { default as BackArrow } from '@mui/icons-material/ArrowBackIos';
+import { Box, Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 import { ChangeEventHandler, FormEventHandler, useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import DashInput from '../components/DashInput';
 import useUser from '../contexts/UserContext';
 import userResource from '../services/api/userResource';
 
@@ -58,14 +59,38 @@ export default function UserDetail() {
 
   return (
     <main>
-      <Link to="/dash/users">
-        <Button startIcon={<ArrowBackIosIcon />}>
-          Back
-        </Button>
-      </Link>
-      <Typography variant="h1">
-        {user ? `User: ${user.username}` : 'New User' }
-      </Typography>
+      <Button
+        LinkComponent="a"
+        href="/dash/users"
+        sx={{ '.MuiButton-startIcon': { mr: 0 } }}
+        onClick={(e) => {
+          e.preventDefault()
+          navigate('/dash/users')
+        }}
+      >
+        <BackArrow sx={{ fontSize: '16px' }} />
+        Back
+      </Button>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end', mt: 1, mb: 7.5 }}>
+        <Typography
+          component="span"
+          color="primary"
+          sx={{
+            fontSize: '20px',
+            fontWeight: 700,
+            '&::after': {
+              content: '"/"',
+              display: 'inline-block',
+              paddingInline: '0.5ch',
+            }
+          }}
+        >
+          Users
+        </Typography>
+        <Typography variant="h1" color="primary">
+          {user ? `${user.username}` : 'New User' }
+        </Typography>
+      </Box>
       <Card
         component="form"
         onSubmit={handleSubmit}
@@ -74,49 +99,72 @@ export default function UserDetail() {
           sx={{
             display: 'flex',
             flexDirection: 'column',
-            gap: 1.5,
+            gap: 2.5,
+            pt: 4.5,
+            pr: 3.75,
+            pb: 1.75,
+            pl: 3.75,
           }}
         >
-          {user && (<TextField
+          <Typography variant="h2" color="primary">
+            {user ? 'Edit user' : 'Create user'}
+          </Typography>
+          {user && (<DashInput
             id="id"
             label="id"
             name="id"
+            size="small"
             value={user?.id}
             inputProps={{
               readOnly: true
             }}
           />)}
-          <TextField
+          <DashInput
             id="username"
             label="username"
             name="username"
+            size="small"
             value={formData.username}
             onChange={handleFormChange}
           />
-          <TextField
+          <DashInput
             id="password"
             label="password"
             name="password"
+            size="small"
             value={formData.password}
             onChange={handleFormChange}
           />
-          <TextField
+          <DashInput
             id="email"
             label="email"
             name="email"
+            size="small"
             value={formData.email}
             onChange={handleFormChange}
           />
         </CardContent>
-        <CardActions>
-          <Link to="/dash/users">
-            <Button>
+        <CardActions sx={{ p: 3.75 }}>
+          <Box sx={{ ml: 'auto' }}>
+            <Button
+              component="a"
+              href="/dash/users"
+              variant="outlined"
+              onClick={(e) => {
+                e.preventDefault()
+                navigate('/dash/users')
+              }}
+            >
               Cancel
             </Button>
-          </Link>          
-          <Button type="submit">
-            Submit
-          </Button>
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ ml: 3.75 }}
+            >
+              Submit
+            </Button>
+          </Box>
         </CardActions>
       </Card>
     </main>
